6f1390b243c6930a7117ab8a96f6876f1c0fab09
Tucker Steil <tuckersteil@gmail.com>
2022-06-28 11:48:36 -0400

    commit tree f134c60b357b79775f26b7e4e5834fae9404f648

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Filter({selectedCategory, onCategoryChange}){
+
+    return(
+        <select name="filter" onChange={onCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+    )
+}
+
+export default Filter;
